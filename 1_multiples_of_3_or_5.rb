1a2946dcb2666b648ed3c8503fc8c1a8b8e528f3
paopapaopao <paolo.casugay@gmail.com>
2021-08-19 00:27:34 +0800

    Update: applied rubocop ruby style guide conventions

--- 1_multiples_of_3_or_5.rb
+++ 1_multiples_of_3_or_5.rb
@@ -1,10 +1,8 @@
 def multiples_of_3_or_5(x)
-	multiples = Array(3...x).select do |n|
-		n % 3 == 0 || n % 5 == 0
-	end
+  multiples = Array(3...x).select { |n| n % 3 == 0 || n % 5 == 0 }
 
-	return multiples.sum
+  return multiples
 end
 
 # 233168
-p multiples_of_3_or_5(1000)
+p multiples_of_3_or_5(1000).sum

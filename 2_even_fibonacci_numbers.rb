4485c4a539f062afe89b76ac7f44e432446e9171
paopapaopao <paolo.casugay@gmail.com>
2021-08-19 00:39:30 +0800

    Update: applied rubocop ruby style guide conventions

--- 2_even_fibonacci_numbers.rb
+++ 2_even_fibonacci_numbers.rb
@@ -1,15 +1,12 @@
 def even_fibonacci_numbers(x)
-	sequence = [1, 2]
-	while sequence[-1] + sequence[-2] <= x
-		sequence << sequence[-1] + sequence[-2]
-	end
+  sequence = [1, 2]
+  while sequence[-1] + sequence[-2] <= x
+    sequence << sequence[-1] + sequence[-2]
+  end
+  evens = sequence.select { |n| n.even? }
 
-	evens = sequence.select do |n|
-		n.even?
-	end
-
-	return evens.sum
+  return evens
 end
 
 # 4613732
-p even_fibonacci_numbers(4000000)
+p even_fibonacci_numbers(4000000).sum

b0d886840afae364928eb9e92093452da1f33e67
paopapaopao <paolo.casugay@gmail.com>
2021-08-19 12:33:56 +0800

    Update: applied rubocop ruby style guide conventions

--- 2_even_fibonacci_numbers.rb
+++ 2_even_fibonacci_numbers.rb
@@ -2,7 +2,7 @@ def even_fibonacci_numbers(x)
   sequence = [1, 2]
   sequence << sequence[-1] + sequence[-2] while sequence[-1] + sequence[-2] <= x
 
-  return sequence.select { |n| n.even? }
+  return sequence.select(&:even?)
 end
 
 # 4613732

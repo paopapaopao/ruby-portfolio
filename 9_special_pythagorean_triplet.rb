ad118471729f4e4cb95f3e595e73ea94c6675de4
paopapaopao <paolo.casugay@gmail.com>
2021-08-23 15:49:28 +0800

    Update: applied rubocop ruby style guide conventions

--- 9_special_pythagorean_triplet.rb
+++ 9_special_pythagorean_triplet.rb
@@ -1,15 +1,11 @@
 def special_pythagorean_triplet(x)
-	triplets = []
-	Array(1...x / 3).each do |a|
-		Array(a + 1..(x - a) / 2).each do |b|
-			c = x - a - b
-			if b < c && a ** 2 + b ** 2 == c ** 2
-				triplets << [a, b, c]
-			end
-		end
-	end
+  Array(1...x / 3).each do |a|
+    Array(a + 1..(x - a) / 2).each do |b|
+      c = x - a - b
 
-	return triplets.first.reduce(:*)
+      return a * b * c if b < c && a**2 + b**2 == c**2 && a + b + c == x
+    end
+  end
 end
 
 # # 31875000

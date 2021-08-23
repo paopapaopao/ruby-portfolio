542107092408cb7cad729926c50f3042f88cab37
paopapaopao <paolo.casugay@gmail.com>
2021-08-23 16:18:50 +0800

    Initial commit

---
+++ 28_number_spiral_diagonals.rb
@@ -0,0 +1,11 @@
+def number_spiral_diagonals(x)
+	diagonals = [1]
+	(3..x).step(2) do |i|
+		Array(0..3).each { |j| diagonals << i**2 - (j * (i - 1)) }
+	end
+
+	return diagonals.sum
+end
+
+# 669171001
+p number_spiral_diagonals(1001)

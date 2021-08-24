80ae6d2b50f162b32cd1e4d2881f9c3eec1ba637
paopapaopao <paolo.casugay@gmail.com>
2021-08-24 16:06:11 +0800

    Update: applied rubocop ruby style guide conventions

--- 25_thousand_digit_fibonacci_number.rb
+++ 25_thousand_digit_fibonacci_number.rb
@@ -1,10 +1,8 @@
-def thousand_digit_fibonacci_number(length)
-	fibo_seq = [1, 1]
-	while fibo_seq[-1].digits.size != length
-		fibo_seq << fibo_seq[-2] + fibo_seq[-1]
-	end
+def thousand_digit_fibonacci_number(x)
+  fibo_seq = [1, 1]
+  fibo_seq << fibo_seq[-2] + fibo_seq[-1] while fibo_seq[-1].digits.size != x
 
-	return fibo_seq.size
+  return fibo_seq.size
 end
 
 # 4782

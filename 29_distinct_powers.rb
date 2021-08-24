494cbaf1997fdcbee062e0c7bd155e6cfc107fa0
paopapaopao <paolo.casugay@gmail.com>
2021-08-24 15:51:26 +0800

    Update: applied rubocop ruby style guide conventions

--- 29_distinct_powers.rb
+++ 29_distinct_powers.rb
@@ -1,13 +1,11 @@
-def distinct_powers(lower_limit, upper_limit)
-	terms = []
-	Array(lower_limit..upper_limit).each do |i|
-		Array(lower_limit..upper_limit).each do |j|
-			terms << i ** j
-		end
-	end
+def distinct_powers(x)
+  terms = []
+  Array(2..x).each do |i|
+    terms += Array(2..x).map { |j| i**j }
+  end
 
-	return terms.uniq.size
+  return terms.uniq.size
 end
 
 # 9183
-p distinct_powers(2, 100)
+p distinct_powers(100)

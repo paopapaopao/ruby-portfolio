1611eefe87e29647fad02407649e9b98bd31a3b9
paopapaopao <paolo.casugay@gmail.com>
2021-08-25 14:42:56 +0800

    Update: applied rubocop ruby style guide conventions

--- 14_longest_collatz_sequence.rb
+++ 14_longest_collatz_sequence.rb
@@ -1,17 +1,16 @@
 def longest_collatz_sequence(x)
-	integer_sequences = []
-	lengths = []
-	Array(1...x).each do |n|
-		terms = [n]
-		while n != 1
-			n = n.even? ? n / 2 : 3 * n + 1
-			terms << n
-		end
-		integer_sequences << terms
-		lengths << terms.length
-	end
+  lengths = []
+  Array(1...x).each do |n|
+    terms = [n]
+    while n != 1
+      n = n.even? ? n / 2 : 3 * n + 1
+      terms << n
+    end
 
-	return lengths.each_with_index.max.last + 1
+    lengths << terms.length
+  end
+
+  return lengths.each_with_index.max.last + 1
 end
 
 # 837799

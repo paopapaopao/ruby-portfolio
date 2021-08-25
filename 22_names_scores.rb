7a420cdcbed6ff2391c75de7def6ab21c91b0a43
paopapaopao <paolo.casugay@gmail.com>
2021-08-25 15:23:50 +0800

    Update: applied rubocop ruby style guide conventions

--- 22_names_scores.rb
+++ 22_names_scores.rb
@@ -1,25 +1,11 @@
 def names_scores(file_name)
-	names = File.read(file_name).split(',')
+  names = File.read(file_name).split(',').map { |name| name[1, name.length - 2] }
+  scores = names.sort.map.each_with_index do |name, index|
+    name.split('').map { |letter| letter.ord - 64 }.sum * (index + 1)
+  end
 
-	names = names.sort.map do |name|
-		name[1, name.length - 2]
-	end
-
-	scores = names.map do |name|
-		sum = 0
-		name.split('').each do |letter|
-			sum += letter.ord - 64
-		end
-		sum
-	end
-
-	new_scores = []
-	scores = scores.each_with_index do |score, index|
-		new_scores << score * (index + 1)
-	end
-
-	puts new_scores.sum
+  return scores.sum
 end
 
 # 871198282
-names_scores('names.txt')
+p names_scores('names.txt')

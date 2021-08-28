1a72669afabbb9bc2aea731d59823424736b2293
paopapaopao <paolo.casugay@gmail.com>
2021-08-29 00:39:23 +0800

    Update: applied rubocop ruby style guide conventions

--- 35_circular_primes.rb
+++ 35_circular_primes.rb
@@ -1,22 +1,20 @@
 require 'prime'
 
-def circular_primes(limit)
-	circulars = []
-	Prime.each(limit - 1) do |i|
-		count = 1
-		prime = i.digits.reverse
-		Array(1...prime.size).each do |j|
-			prime.rotate!
+def circular_primes(x)
+  circulars = []
+  Prime.each(x - 1) do |p|
+    count = 1
+    prime = p.digits.reverse
+    Array(1...prime.size).each do |j|
+      prime.rotate!
 
-			prime.join.to_i.prime? ? count += 1 : break
-		end
+      prime.join.to_i.prime? ? count += 1 : break
+    end
 
-		if count == i.digits.size
-			circulars << i
-		end
-	end
+    circulars << p if count == p.digits.size
+  end
 
-	return circulars.size
+  return circulars.size
 end
 
 # 55

01781b86a86916c6116a6192b73db6bdcae0762a
paopapaopao <paolo.casugay@gmail.com>
2021-09-05 10:51:11 +0800

    Update: applied rubocop ruby style guide conventions

--- 12_highly_divisible_triangular_number.rb
+++ 12_highly_divisible_triangular_number.rb
@@ -1,22 +1,18 @@
 def highly_divisible_triangular_number(x)
-	triangle_factors = {}
-	triangle = 0
-	loop do
-		triangle += triangle_factors.size + 1
-		factors = []
-		Array(1..Math.sqrt(triangle)).each do |n|
-			if triangle % n == 0
-				factors << n << triangle / n
-			end
-		end
-		triangle_factors[triangle] = factors.uniq.sort
+  triangle_factors = {}
+  triangle = 0
+  loop do
+    triangle += triangle_factors.size + 1
+    factors = []
+    Array(1..Math.sqrt(triangle)).each do |n|
+      factors += [n, triangle / n] if triangle % n == 0
+    end
+    triangle_factors[triangle] = factors.uniq.sort
 
-		if factors.size > x
-			break
-		end
-	end
+    break if factors.size > x
+  end
 
-	return triangle_factors.keys.last
+  return triangle_factors.keys.last
 end
 
 # 76576500

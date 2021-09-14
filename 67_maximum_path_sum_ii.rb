ca26ecd86896fa96dbcad971e124ca2dd78430a9
paopapaopao <paolo.casugay@gmail.com>
2021-09-14 21:21:31 +0800

    Update: applied rubocop ruby style guide conventions

--- 67_maximum_path_sum_ii.rb
+++ 67_maximum_path_sum_ii.rb
@@ -1,15 +1,14 @@
 def maximum_path_sum_ii(file_name)
-	triangle = []
-	File.read(file_name).split("\n").each do |row|
-		triangle << row.split(" ").map(&:to_i)
-	end
+  triangle = File.read(file_name).split("\n").map do |row|
+    row.split(" ").map(&:to_i)
+  end
 
-	path_sums = triangle.last
-	Array(0...triangle.size - 1).reverse.each do |row|
-		path_sums = triangle[row].each_with_index.map { |n, i| n + path_sums[i, 2].max }
-	end
+  path_sums = triangle.last
+  Array(0...triangle.size - 1).reverse.each do |row|
+    path_sums = triangle[row].each_with_index.map { |n, i| n + path_sums[i, 2].max }
+  end
 
-	return path_sums.first
+  return path_sums.first
 end
 
 # 7273

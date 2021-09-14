83d30e9ac226c06d159014d25c1cd4373d5cc19e
paopapaopao <paolo.casugay@gmail.com>
2021-09-14 21:26:05 +0800

    Update: applied rubocop ruby style guide conventions

--- 21_amicable_numbers.rb
+++ 21_amicable_numbers.rb
@@ -1,27 +1,27 @@
 def amicable_numbers(x)
-	proper_divisors = [0]
-	Array(1...x).each do |n|
-		divisors = []
-		Array(1..Math.sqrt(n)).each do |i|
-			divisors << i if n % i == 0
-			divisors << n / i if n % i == 0 && i != 1
-		end
-		proper_divisors << divisors.uniq
-	end
+  proper_divisors = [0]
+  Array(1...x).each do |n|
+    divisors = []
+    Array(1..Math.sqrt(n)).each do |i|
+      divisors << i if n % i == 0
+      divisors << n / i if n % i == 0 && i != 1
+    end
+    proper_divisors << divisors.uniq
+  end
 
-	amicables = []
-	Array(1...x).each do |n|
-		b = proper_divisors[n].sum
-		next if b > proper_divisors.size
-		a = proper_divisors[b].sum
-		next if a > proper_divisors.size
+  amicables = []
+  Array(1...x).each do |n|
+    b = proper_divisors[n].sum
+    next if b > proper_divisors.size
+    a = proper_divisors[b].sum
+    next if a > proper_divisors.size
 
-		if proper_divisors[a].sum == b && proper_divisors[b].sum == a && a != b
-		  amicables += [a, b]
-		end
-	end
+    if proper_divisors[a].sum == b && proper_divisors[b].sum == a && a != b
+      amicables += [a, b]
+    end
+  end
 
-	return amicables.uniq.sum
+  return amicables.uniq.sum
 end
 
 # 31626
